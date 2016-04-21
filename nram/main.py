67c13e5ad1322d2322e8ff44777d67567304628d
Michael Vogelsong <mjvogelsong@gmail,com>
2016-04-21 14:17:22 -0700

    runs without error, although it's using dummy data

--- nram/main.py
+++ nram/main.py
@@ -1,4 +1,5 @@
 import os
+import pprint
 
 import tensorflow as tf
 
@@ -10,13 +11,13 @@ flags.DEFINE_integer("epoch",
     25,
     "Epoch to train [25]")
 flags.DEFINE_string("checkpoint_dir",
-    "checkpoint",
+    "ckpt",
     "Directory name to save the checkpoints [checkpoint]")
 flags.DEFINE_string("sample_dir",
     "samples",
     "Directory name to save the image samples [samples]")
 flags.DEFINE_boolean("is_train",
-    False,
+    True,
     "True for training, False for testing [False]")
 flags.DEFINE_integer("batch_size",
     1,
@@ -43,27 +44,29 @@ flags.DEFINE_integer("num_h1_units",
     128,
     "Number of hidden units in layer 1")
 flags.DEFINE_integer("R_num_registers",
-    16,
+    4,
     "Number of registers")
 flags.DEFINE_integer("M_num_ints",
-    32,
+    12,
     "Number of integers representable in memory")
 flags.DEFINE_integer("Q_num_modules",
     2,
     "Number of compute modules")
-flags.DEFINE_integer("num_h1_units",
-    128,
-    "Number of hidden units in layer 1")
+flags.DEFINE_integer("max_minibatches",
+    1,
+    "Maximum number of minibatches to run")
 
 FLAGS = flags.FLAGS
 
+
 def main(_):
-    print FLAGS
+    pprint.PrettyPrinter().pprint(FLAGS.__flags)
+    '''
     if not os.path.exists(FLAGS.checkpoint_dir):
         os.makedirs(FLAGS.checkpoint_dir)
     if not os.path.exists(FLAGS.sample_dir):
         os.makedirs(FLAGS.sample_dir)
-
+    '''
     m = model.Model(FLAGS)
 
     if FLAGS.is_train:
--- nram/model.py
+++ nram/model.py
@@ -2,28 +2,25 @@ import math
 import time
 
 import tensorflow as tf
+import numpy as np
 
+# pylint: disable=E1103
 class Model(object):
     def __init__(self, flags):
-        self.saver = tf.train.Saver()
-
         self.batch_size = flags.batch_size
         self.num_epochs = flags.num_epochs
         self.train_dir = flags.train_dir
-        self.max_steps = flags.max_steps
+        self.max_minibatches = flags.max_minibatches
         self.report_interval = flags.report_interval
         self.checkpoint_interval = flags.checkpoint_interval
         self.alpha_learning_rate = flags.alpha_learning_rate
         self.num_h1_units = flags.num_h1_units
+        self.R_num_registers = flags.R_num_registers
+        self.M_num_ints = flags.M_num_ints
+        self.Q_num_modules = flags.Q_num_modules
+        self.T_max_timesteps = flags.T_max_timesteps
 
-        self._build(flags)
-
-    def _build(self, flags):
-        self.R_num_registers = tf.constant(flags.R_num_registers, name="R_num_registers")
-        self.M_num_ints = tf.constant(flags.M_num_ints, name="M_num_ints")
-        self.Q_num_modules = tf.constant(flags.Q_num_modules, name="Q_num_modules")
-        self.T_max_timesteps = tf.constant(flags.T_max_timesteps, name="T_max_timesteps")
-
+    def _build(self):
         self.r_registers = tf.get_variable("r_registers",
             [self.R_num_registers, self.M_num_ints],
             initializer=tf.constant_initializer(0.0))
@@ -38,55 +35,68 @@ class Model(object):
             initializer=tf.constant_initializer(0.0))
 
     def train(self):
-        data_sets = data.read_data_sets(self.train_dir)
+        data_sets = np.zeros([self.M_num_ints, self.M_num_ints])
+        targets = np.ones([self.M_num_ints])
         with tf.Graph().as_default():
+            self._build()
             input_pl, targets_pl = self._placeholder_inputs(self.batch_size)
+            print("input_pl: %s" % input_pl)
+            print("targets_pl: %s" % targets_pl)
             loss = self._controller(input_pl, targets_pl)
             train_op = self._training(loss, self.alpha_learning_rate)
-            eval_correct = self._evaluation(logits, targets_pl)
+            #eval_correct = self._evaluation(logits, targets_pl)
             summary_op = tf.merge_all_summaries()
-            with tf.Session() as sess:
-                init = tf.initialize_all_variables()
-                sess.run(init)
-                summary_writer = tf.train.SummaryWriter(self.train_dir, sess.graph)
-                for step in xrange(self.max_steps):
-                    start_time = time.time()
-                    feed_dict = self._fill_feed_dict(data_sets.train, input_pl, targets_pl)
-                    _, loss_value = sess.run([train_op, loss], feed_dict=feed_dict)
-                    duration = time.time() - start_time
+            saver = tf.train.Saver()
+            sess = tf.Session()
+            init = tf.initialize_all_variables()
+            sess.run(init)
+            summary_writer = tf.train.SummaryWriter(self.train_dir, sess.graph)
+            for step in xrange(self.max_minibatches):
+                start_time = time.time()
+                #feed_dict = self._fill_feed_dict(data_sets.train, input_pl, targets_pl)
 
-                    if step % self.report_interval == 0:
-                        print('Step %d: loss = %.2f (%.3f sec)' % (step, loss_value, duration))
+                feed_dict = {
+                    input_pl: data_sets,
+                    targets_pl: targets
+                }
+                _, loss_value = sess.run([train_op, loss], feed_dict=feed_dict)
+                duration = time.time() - start_time
 
-                        summary_str = sess.run(summary_op, feed_dict=feed_dict)
-                        summary_writer.add_summary(summary_str, step)
-                        summary_writer.flush()
+                if step % self.report_interval == 0:
+                    print('Step %d: loss = %.2f (%.3f sec)' % (step, loss_value, duration))
+                    '''
+                    summary_str = sess.run(summary_op, feed_dict=feed_dict)
+                    summary_writer.add_summary(summary_str, step)
+                    summary_writer.flush()
+                    '''
 
-                    # Save a checkpoint and evaluate the model periodically.
-                    if (step + 1) % self.checkpoint_interval == 0 \
-                            or (step + 1) == self.max_steps:
-                        self.saver.save(sess, self.train_dir, global_step=step)
-                        # Evaluate against the training set.
-                        print('Training Data Eval:')
-                        self._do_eval(sess,
-                                eval_correct,
-                                input_pl,
-                                targets_pl,
-                                data_sets.train)
-                        # Evaluate against the validation set.
-                        print('Validation Data Eval:')
-                        self._do_eval(sess,
-                                eval_correct,
-                                input_pl,
-                                targets_pl,
-                                data_sets.validation)
-                        # Evaluate against the test set.
-                        print('Test Data Eval:')
-                        self._do_eval(sess,
-                                eval_correct,
-                                input_pl,
-                                targets_pl,
-                                data_sets.test)
+                # Save a checkpoint and evaluate the model periodically.
+                if (step + 1) % self.checkpoint_interval == 0 \
+                        or (step + 1) == self.max_minibatches:
+                    saver.save(sess, self.train_dir, global_step=step)
+                    # Evaluate against the training set.
+                    '''
+                    print('Training Data Eval:')
+                    self._do_eval(sess,
+                            eval_correct,
+                            input_pl,
+                            targets_pl,
+                            data_sets.train)
+                    # Evaluate against the validation set.
+                    print('Validation Data Eval:')
+                    self._do_eval(sess,
+                            eval_correct,
+                            input_pl,
+                            targets_pl,
+                            data_sets.validation)
+                    # Evaluate against the test set.
+                    print('Test Data Eval:')
+                    self._do_eval(sess,
+                            eval_correct,
+                            input_pl,
+                            targets_pl,
+                            data_sets.test)
+                    '''
 
 
     def save(self, sess, save_path, global_step):
@@ -129,13 +139,13 @@ class Model(object):
         return feed_dict
 
     def _placeholder_inputs(self, batch_size):
-        input_data = tf.placeholder(tf.float32, shape=(batch_size, self.M_num_ints))
-        target_data = tf.placeholder(tf.float32, shape=(batch_size, self.M_num_ints))
+        input_data = tf.placeholder(tf.float32, shape=(self.M_num_ints, self.M_num_ints))
+        target_data = tf.placeholder(tf.float32, shape=(self.M_num_ints))
         return input_data, target_data
 
     def _training(self, loss, learning_rate):
         # Add a scalar summary for the snapshot loss.
-        tf.scalar_summary(loss.op.name, loss)
+        #tf.scalar_summary(loss.op.name, loss)
         # Create the gradient descent optimizer with the given learning rate.
         optimizer = tf.train.AdamOptimizer(learning_rate)
         # Create a variable to track the global step.
@@ -146,7 +156,7 @@ class Model(object):
         return train_op
 
     def _intermediate_controller(self, register_output):
-        inputs = self._interpret_register(register_output)
+        inputs = tf.transpose(self._interpret_register(register_output))
 
         with tf.name_scope("h1"):
             w = tf.Variable(tf.truncated_normal([self.R_num_registers, self.num_h1_units],
@@ -154,6 +164,9 @@ class Model(object):
                 name="w")
             b = tf.Variable(tf.zeros([self.num_h1_units]),
                 name="b")
+            print("w: %s" % w)
+            print("b: %s" % b)
+            print("inputs: %s" % inputs)
             h1 = tf.nn.relu(tf.matmul(inputs, w) + b)
 
         with tf.name_scope("a"):
@@ -185,10 +198,10 @@ class Model(object):
 
         with tf.name_scope("f"):
             w = tf.Variable(tf.truncated_normal(
-                [self.num_h1_units, (self.R_num_registers + self.Q_num_modules)],
+                [self.num_h1_units, 1],
                 stddev=1.0 / math.sqrt(float(self.num_h1_units))),
                 name="w")
-            b = tf.Variable(tf.zeros([(self.R_num_registers + self.Q_num_modules)]),
+            b = tf.Variable(tf.zeros([1]),
                 name="b")
             logit_x = tf.matmul(h1, w) + b
             f = tf.nn.sigmoid(logit_x)
@@ -203,34 +216,48 @@ class Model(object):
         loss = tf.Variable(tf.zeros([1]))
 
         for step in xrange(self.T_max_timesteps):
-            local_memory = \
-                tf.concat(0, [local_memory[:self.R_num_registers], tf.zeros([self.Q_num_modules, self.M_num_ints])])
+            local_memory = self.r_registers
             for mod_idx in xrange(self.Q_num_modules):
                 with tf.variable_scope("module") as scope:
                     scope.reuse_variables()
-                    a, b, _, _ = self._intermediate_controller(local_memory)
+                    a, b, _, _ = self._intermediate_controller(self.r_registers)
+                    print("a: %s" % a)
+                    print("b: %s" % b)
+                    zero_padding = tf.zeros([self.Q_num_modules - mod_idx, self.M_num_ints])
+                    print("zero_padding: %s" % zero_padding)
+                    print("local_memory: %s" % local_memory)
+                    local_memory_padded = tf.concat(0, [local_memory, zero_padding])
+                    print("local_memory_padded: %s" % local_memory_padded)
+                    o_i = self._module_function(mod_idx,
+                        tf.matmul(tf.transpose(local_memory_padded),
+                            tf.transpose(tf.nn.softmax(a))),
+                        tf.matmul(tf.transpose(local_memory_padded),
+                            tf.transpose(tf.nn.softmax(b))))
+                    print("o_i: %s" % o_i)
+                    local_memory = tf.concat(0, [local_memory, tf.transpose(o_i)])
 
-                    module_l = \
-                        tf.matmul(tf.transpose(local_memory), tf.nn.softmax(a))
-                    module_r = \
-                        tf.matmul(tf.transpose(local_memory), tf.nn.softmax(b))
-                    local_memory[self.R_num_registers + mod_idx] = \
-                        self._module_function(mod_idx, module_l, module_r)
-
-            temp_local_memory = tf.Variable(tf.zeros([self.R_num_registers, self.M_num_ints]))
             for reg_idx in xrange(self.R_num_registers):
                 with tf.variable_scope("module") as scope:
-                    _, _, c, _ = self._intermediate_controller(local_memory)
-                    temp_local_memory[reg_idx] = \
-                        tf.matmul(tf.transpose(local_memory), tf.nn.softmax(c))
+                    _, _, c, _ = self._intermediate_controller(self.r_registers)
+                    r_i = tf.matmul(tf.transpose(local_memory), tf.transpose(tf.nn.softmax(c)))
+                    print("r_i: %s" % r_i)
+                    if reg_idx == 0:
+                        temp_r_registers = tf.transpose(r_i)
+                    else:
+                        temp_r_registers = tf.concat(0, [temp_r_registers, tf.transpose(r_i)])
+                    print("temp_r_registers: %s:" % temp_r_registers)
 
-            _, _, _, f = self._intermediate_controller(local_memory)
-
-            local_memory = temp_local_memory
-
+            _, _, _, f = self._intermediate_controller(self.r_registers)
+            print("f: %s" % f)
+            print("temp_r_registers: %s" % temp_r_registers)
+            self.r_registers.assign(temp_r_registers)
+            print("self.r_registers: %s" % self.r_registers)
+            print("p_before: %s" % p)
             p = (1.0 - p) * f
-
-            loss -= p * tf.log(tf.reduce_sum(labels_pl[step] * self.BIGM_memory))
+            print("p_after: %s" % p)
+            print("labels_pl: %s" % labels_pl)
+            print("self.BIGM_memory: %s" % self.BIGM_memory)
+            loss -= p * tf.log(tf.reduce_sum(labels_pl * self.BIGM_memory))
 
             '''
             if tf.random_uniform([0.0, 1.0]) <= f:
@@ -240,10 +267,7 @@ class Model(object):
         return loss
 
     def _init_memory(self, int_array):
-        self.BIGM_memory = tf.zeros(size=(self.M_num_ints, self.M_num_ints))
-        for i, val in enumerate(int_array):
-            self.BIGM_memory[i][val] = 1.0
-        return len(int_array)
+        self.BIGM_memory = int_array
 
     def _module_function(self, fun_idx, a, b):
         if fun_idx == 0:
@@ -252,6 +276,7 @@ class Model(object):
             return self._module_WRITE(a, b)
 
     def _interpret_register(self, registers):
+        print("registers: %s" % (registers))
         return tf.slice(registers, [0, 0], [-1, 1])
 
     def _read_memory(self):
@@ -261,8 +286,12 @@ class Model(object):
         return tf.matmul(tf.transpose(self.BIGM_memory), p_pointer)
 
     def _module_WRITE(self, p_pointer, a_value):
-        J_ones = tf.ones([self.M_num_ints])
+        J_ones = tf.ones([self.M_num_ints, 1])
+        print("p_pointer: %s" % p_pointer)
+        print("a_value: %s" % a_value)
+        print("J_ones: %s" % J_ones)
+        print("self.BIGM_memory: %s" % self.BIGM_memory)
         self.BIGM_memory = \
             (tf.matmul(J_ones - p_pointer, tf.transpose(J_ones)) * self.BIGM_memory) + \
             tf.matmul(p_pointer, tf.transpose(a_value))
-        return tf.zeros([self.M_num_ints])
+        return tf.zeros([self.M_num_ints, 1])
